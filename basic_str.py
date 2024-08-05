68d442d963e1cf7545d141ab565ce7da89ea7cba
JJIWON <hello093093@gmail.com>
2024-08-05 09:28:19 +0900

    2024/08/02

---
+++ basic_str.py
@@ -0,0 +1,132 @@
+def define_str():
+    """
+    함수 정의 연습
+    """
+    # 문자열의 정의
+    # 한 줄 문자열: 쌍따옴표("), 홀따옴표(') 모두 가능
+    s1 = "Hello Python" # 리터럴 생성
+    s2 = str("Hello Python")    # 타입 함수 사용
+    s3 = str(3.14159)   # 다른 타입을 변환 생성
+
+    print(s1, type(s1))
+    print(s2, type(s2))
+    print(s3, type(s3))
+
+    print("s1은 str?", isinstance(s1, str))
+
+    # 여러 줄 문자열: """, '''
+    s4 = """Life is too short, You need Python.
+파이썬은 데이터 처리가 중요한 시대에서
+가장 널리 사용되는 언어입니다."""
+
+    print(s4)
+
+    # 여러 줄 문자열은 한줄 주석만 있는 파이썬에서
+    # 여러 줄 주석을 사용하고자 할 때도 사용할 수 있다.
+    """여러 줄 문자열을 사용한 여러 줄 주석
+    메서드 정의 바로 아래 여러 줄 주석을 추가하면
+    문서화 할 때 이용될 수 있고
+    help 명령어로 해당 메서드의 주석을 볼 수 있다."""
+
+    # f-string : 문자열 포맷팅 방식 중 하나
+    name, age = "홍길동", 28
+    message = f"안녕하세요, {name}님. 당신은 {age}살 입니다."
+
+    print(message)
+
+    width, height = 10, 5
+    message = f"사각형의 면적은 {width * height}입니다."
+
+    print(message)
+
+
+
+def string_oper():
+    """
+    문자열 연산
+    """
+
+    str1 = "First String"
+    str2 = "Second Strnig"
+    # len(), 인덱싱 가능, 슬라이싱 가능, 포함여부 판별,
+    # immutable, 치환 불가능
+    print(len(str1), len(str2)) # 길이
+
+    # 인덱싱
+    print(str1[0], str1[1], str1[2], "...", str1[9], str1[10], str1[11])    # 정방향
+    print(str1[-12], str1[-11], str1[-10], "...",
+          str1[-3], str1[-2], str1[-1])
+
+    # 문자열은 immutable -> 치환 불가
+    # str1[0] = "f" # 변경 불가
+
+    # 슬라이싱
+    str1 = "First String"
+    # [시작경계:끝경계[:간격]]
+    print(str1)
+    print(str1[6:9])    # 정방향 인덱스 활용
+    print(str1[-6:-3])  # 역방향 인덱스 활용
+
+    print(str1[0:5])
+    print(str1[:5]) # 시작부터 -> 생략 가능
+
+    print(str1[6:len(str1)])
+    print(str1[6:]) # 끝까지 -> 생략 가능
+
+    print(str1[::2])    # 처음부터 끝까지 간격 2
+    print(str1[::-1])   # 간격 값이 음수 -> 역방향
+
+
+def transform_methods():
+    """
+    대소문자 변환 관련 연습
+    """
+    s = "i like python"
+
+    print("UPPER:", s.upper())  # 모두 대문자
+    print("LOWER:", s.lower())  # 모두 소문자로
+    print("CAPITALIZE:", s.capitalize())    # 첫 글자만 대문자로
+    print("TITLE:", s.title())  # 각 단어의 첫 글자만 대문자로
+
+    print("SWAPCASE:", s.title().swapcase())    # 대 <-> 소
+
+    # 불변 자료 -> 원본은 바뀌지 않는다.
+    print("원본:", s)
+
+
+def search_methods():
+    """
+    문자열 검색 관련 예제
+    """
+    s = "I Like Python, I Like Java Also"
+    print("COUNT:", s.count("Like"))    #  문자열 내부의 Like의 갯수
+
+    # Like을 찾아봅시다. : find
+    index = s.find("Like")  # 문자열 내부의 첫 번째 Like
+    print("1st Find:", index)
+    index = s.find("Like", 6)   # 인덱스 6부터 검색
+    print("2nd Find:", index)
+    index = s.find("Like", 21)  # 인덱스 21부터 검색
+    print("3rd Find:", index)   # 찾을 검색어가 없으면 -1
+
+    # Like을 찾아봅시다. : index
+    print("1st Index:", s.index("Like"))
+    print("2nd Index:", s.index("Like", 6))
+    # print("3rd Index:", s.index("Like", 21))  # Error!
+    # 방법 1: 예외처리
+    # 방법 2: 먼저 검색어 포함 여부를 확인 후  검색
+    if "Like" in s[21:]: # 포함 여부
+        print("3rd Index:", s.index("Like", 21))
+    else:
+        print("21번 인덱스 이후에는 Like 없음")
+
+    # 역방향 검색 : find
+    print("RFIND:", s.rfind("Like"))    # 17
+    print("RFIND:", s.rfind("Like", 0, 17))
+
+
+if __name__ == "__main__":
+    # define_str()
+    # string_oper()
+    # transform_methods()
+    search_methods()
--- basic_syntax.py
+++ basic_syntax.py
@@ -159,6 +159,63 @@ def type_hint():
     print(typed)
 
 
+def int_ex():
+    print("======== 정수형 연습")
+
+    a = 2024    # 리터럴 선언
+    b = int(2024)   # 타입 함수 사용
+
+    print(a, "is", type(a))
+    print(b, "is", type(b))
+
+    # 2진, 8진, 16진 정수 표현 가능
+    b = 0b1101  # 0b
+    o = 0o23    # 0o
+    x = 0xFF    # 0x
+
+    print(b, o, x)
+
+    # 파이썬 3에서는 int와 long 구분 없음
+    # long형 데이터 저장크기 64bit 초과해서 적재할 수 있음
+    i = 2 ** 2048
+    print(i)
+    print(i.bit_length())
+
+    # 진법 변환 함수
+    i = 48
+    print(i, bin(i), oct(i), hex(i))
+
+def float_ex():
+    print("========= 실수형 연습")
+    a = 3.14159 # 리터럴로 생성
+    print(a, "is", type(a))
+    print(isinstance(a, float)) # a는 float인가?
+
+    b = float(3.0)  # 타입 함수로 생성
+    print(b, "is", type(b))
+    print(isinstance(b, float)) # b는 float인가?
+
+    print(a.is_integer(), b.is_integer())
+
+    # 지수 표기법으로도 확인 가능
+    c = 3e3 # 3 * 10 ** 3
+    d = -2E-5   # -2 * 10 ** -5
+    print(c, d)
+    print(-2E-5 == -0.00002)
+
+def complex_ex():
+    print("========= 복소수 연습")
+    # 복소수 : 실수부 + 허수부j
+    a = 4 + 5j
+    print(a, "is", type(a))
+    print(isinstance(a, complex))   # a는 복소수?
+
+    b = 7 - 2J
+    print(a + b)    # 복소수는 산술연산이 가능
+    print(b.real, b.imag)   # 실수부, 허수부
+    print(a, "의 켤레복소수는", a.conjugate()) # 켤레 복소수
+
+
 if __name__ == "__main__":
     # 다른 모듈로 import 되는 경우 __name__ == "python_basics"
     # 직접 실행될 경우(최상위 모듈일 경우) __name__ == "__main__"
@@ -167,4 +224,7 @@ if __name__ == "__main__":
     # var_ex()    # 변수 연습
     # assignment_ex()  # 변수 치환 연습
     # bool_ex()
-    type_hint()
+    # type_hint()
+    # int_ex()
+    # float_ex()
+    complex_ex()
